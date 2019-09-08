9fbfbf6a721c4326c9159054c47f07c6bd2db35d
Krishna Murthy <krishnamurthy.t.h@gmail.com>
2019-09-08 20:56:23 +0530

    Second commit
    Added depencencyes and added signup and login screens

---
+++ app/src/main/java/com/krishna/mvvm/ui/auth/LoginActivity.kt
@@ -0,0 +1,13 @@
+package com.krishna.mvvm.ui.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.krishna.mvvm.R
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
--- app/src/main/java/com/krishna/mvvm/ui/auth/SignupActivity.kt
+++ app/src/main/java/com/krishna/mvvm/ui/auth/SignupActivity.kt
@@ -1,12 +1,13 @@
-package com.krishna.mvvm
+package com.krishna.mvvm.ui.auth
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.krishna.mvvm.R
 
-class MainActivity : AppCompatActivity() {
+class SignupActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_signup)
     }
 }
